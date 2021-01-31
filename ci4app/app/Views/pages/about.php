fed7665e614407fc5c94f333ba55661510617275
AriefBudi18 <kusumah.183040014@mail.unpas.ac.id>
2021-01-31 14:09:45 +0700

    Project 3

---
+++ ci4app/app/Views/pages/about.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+                illo deserunt eum ut libero in, beatae alias provident! In suscipit
+                obcaecati commodi, culpa ullam laudantium? Quo iusto placeat sed
+                quibusdam.</p>
+        </div>
+    </div>
+</div>
